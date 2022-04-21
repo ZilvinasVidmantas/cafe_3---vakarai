242e99fac859854fc37f9a6a0f732c8ee1c67e1a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-21 19:13:59 +0300

    feat(19): cycles

--- JavaScript [17-36]/19 pamoka - funkcijos, ciklai ir masyvų metodai/main.js
+++ JavaScript [17-36]/19 pamoka - funkcijos, ciklai ir masyvų metodai/main.js
@@ -1,1 +1,47 @@
-console.log('veikia');
+/*
+   Laura
+   Tatjana
+   Mažvydas
+   Titas
+   Juozas
+   Radoslav
+*/
+
+
+const numbers = [7, 8, 9, 7, 4, 'nesamone', 5, 4, 1, 8, 4];
+
+console.group('Break pvz:');
+{
+  let i = 0;
+  let sum = 0;
+
+  while (i < numbers.length) {
+    if (typeof numbers[i] !== 'number') {
+      console.error('Blogai duomenys', numbers);
+      break;
+    }
+    sum += numbers[i];
+    i++;
+  }
+
+  console.log('suma:', sum);
+}
+console.groupEnd();
+
+console.group('Continue pvz:');
+{
+  let i = 0;
+  let sum = 0;
+
+  while (i < numbers.length) {
+    if (typeof numbers[i] !== 'number') {
+      i++;
+      continue;
+    }
+    sum += numbers[i];
+    i++;
+  }
+
+  console.log('suma:', sum);
+}
+console.groupEnd();
