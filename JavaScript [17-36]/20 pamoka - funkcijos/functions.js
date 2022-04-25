e401f87039e9510e321b40d2a2a52f1ccdfd029a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-25 19:52:11 +0300

    feat(20): geometry calculation functions

--- JavaScript [17-36]/20 pamoka - funkcijos/functions.js
+++ JavaScript [17-36]/20 pamoka - funkcijos/functions.js
@@ -42,4 +42,3 @@ console.log('trikampioPlotas(8, 7, 65) →', trikampioPlotas(8, 7, 65));
 console.log('2 * staciakampioPlotas(5, 8) - kvadratoPlotas(2) →', 2 * staciakampioPlotas(5, 8) - kvadratoPlotas(2));
 console.log('6 * trikampioPlotas(3, 3, 60) →', 6 * trikampioPlotas(3, 3, 60));
 console.log('staciakampioPlotas(6, 12) →', staciakampioPlotas(6, 12));
-// 
--- JavaScript [17-36]/20 pamoka - funkcijos/main.js
+++ JavaScript [17-36]/20 pamoka - funkcijos/main.js
@@ -1,2 +1,50 @@
-console.log('haiii');
+function calcTriangleArea(b, c, angleA) {
+  return 0.5 * b * c * Math.sin(angleA * Math.PI / 180);
+}
 
+function calcTrianglePerimeter(b, c, angleA) {
+  const a = (b ** 2 + c ** 2 - 2 * b * c * Math.cos(angleA * Math.PI / 180)) ** (1 / 2);
+
+  return a + b + c;
+}
+
+console.group('Triangle props');
+const edge1 = 3;
+const edge2 = 4;
+const angleBetween = 90;
+
+const triangleArea = calcTriangleArea(edge1, edge2, angleBetween);
+const trianglePerimeter = calcTrianglePerimeter(edge1, edge2, angleBetween);
+
+console.table({
+  edge1,
+  edge2,
+  angleBetween,
+  '------': '-------------------',
+  triangleArea,
+  trianglePerimeter,
+});
+
+function calcCircleArea(r) {
+
+}
+
+function calcCirclePerimerter(r) {
+
+}
+
+function calcSquareArea(a) {
+
+}
+
+function calcSquarePerimeter(a) {
+
+}
+
+function calcRectangleArea(a, b) {
+
+}
+
+function calcRectanglePerimeter(a, b) {
+
+}
