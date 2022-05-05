04d2430849335b49c503d82b93f222a0a6d26249
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-05 18:59:29 +0300

    feat(27): lambda and filter explanation

--- JavaScript [17-36]/27 pamoka - filter ir reduce/main.js
+++ JavaScript [17-36]/27 pamoka - filter ir reduce/main.js
@@ -1,74 +1,79 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-const words = ['labas', 'vakaras', 'saulius', 'kūdra'];
-const flats = [{
-  address: 'Kunigų g. 7',
-  rooms: [20, 16, 14], // 50
-  price: 50000,
-}, {
-  address: 'Sargėnų vieta 5',
-  rooms: [20, 16, 14, 20], // 70
-  price: 70000,
-}, {
-  address: 'Jūros pr. 12',
-  rooms: [50, 40], // 90
-  price: 90000,
-}, {
-  address: 'Fantazijo al. 77',
-  rooms: [15, 12, 13, 15, 14], // 69
-  price: 65000,
-}];
-const EUR_USD = 1.06;
+/*
+Array.prototype.filter iteracinis metodas yra skirtas kurti naują masyvą, ATRENKANT REIKŠMES PAGAL SALYGĄ iš iteruojamų masyvo elementų.
+Iteruodamas per kiekvieną masyvo elementą, jis vykdo parametru perduotą funkciją, perduodant jai einamajį elementą, jo indeksą ir 
+iteruojamajį masyvą. Jeigu perduotos funkcijos grąžinimo reikšmė yra 'truthy', einamasis elementas įdedamas į naujai formuojamą masyvą.
+Priešingu atveju, jeigu parametru perduotos funkcijos grąžinimo rezultatas su einamuoju elementu yra 'falsy', tuomet einamasis elementas NĖRA įdedamas į naujai formuojamą masyvą.
+ 
+Kaip ir Array.prototype.forEach atveju, perduodamoji (tikrinimo) funkcija gali priimti 3 parametrus:
+  1. einamasis elementas
+  2. einamojo elemento indeksas
+  3. iteruojamas masyvas
+Perduodamoji funkcija PRI-VA-LO grąžinti BOOLEAN, kur priklausomai nuo jos grąžinimo:
+  * true - elementas bus pridedamas į naujai formuojamą masyvą.
+  * false - elementas NEBUS pridedamas į naujai formuojamą masyvą.
+*/
 
-const double = function (num) {
-  return num * 2;
+// 19:15
+console.group('Veikimo pavyzdys - atrikti lyginius skaičius');
+{
+  const numbers = [1, 2, 3, 4];
+
+  console.group('Naudojant iš anksto aprašytą funkciją');
+  {
+    const isEven = function (el) {
+      return el % 2 === 0;
+    }
+
+    const numbersEven = numbers.filter(isEven);
+    console.log(numbersEven);
+  }
+  console.groupEnd();
+  console.group('Naudojant anoniminę funkciją');
+  {
+    const numbersEven = numbers.filter(function (el) {
+      return el % 2 === 0;
+    });
+    console.log(numbersEven);
+  }
+  console.groupEnd();
+  console.group('Naudojant iš anksto aprašytą lambda išraišką');
+  {
+    const isEven = el => el % 2 === 0;
+
+    const numbersEven = numbers.filter(isEven);
+    console.log(numbersEven);
+  }
+  console.groupEnd();
+  console.group('Naudojant anoniminę lambda išraišką');
+  {
+    const numbersEven = numbers.filter(el => el % 2 === 0);
+    console.log(numbersEven);
+  }
+  console.groupEnd();
 }
+console.groupEnd();
 
-const doubledNumbers = numbers.map(double);
-console.log(doubledNumbers);
+console.group();
+{
+  console.group('Naudojant iš anksto aprašytą funkciją');
+  {
 
-// Sukurkite naują masyvą, pagal masyvą words, kad kiekviena reikšmė būtų didžiosiomis raidėmis
-const toUpperCase = function (word) {
-  return word.toUpperCase();
+  }
+  console.groupEnd();
+  console.group('Naudojant anoniminę funkciją');
+  {
+
+  }
+  console.groupEnd();
+  console.group('Naudojant iš anksto aprašytą lambda išraišką');
+  {
+
+  }
+  console.groupEnd();
+  console.group('Naudojant anoniminę lambda išraišką');
+  {
+
+  }
+  console.groupEnd();
 }
-const wordsInCapital = words.map(toUpperCase);
-console.log(wordsInCapital);
-
-// Sukurkite naują masyvą, pagal masyvą words, kad kiekviena reikšmė prasidėtų iš didžiosios raidės
-
-const capitalize = function (word) {
-  return word[0].toUpperCase() + word.slice(1);
-}
-const wordsCapitalized = words.map(capitalize);
-console.log(wordsCapitalized);
-
-// Sukurkite naują masyvą, pagal masyvą numbers, kad kiekviena reikšmė būtų pakeista lieka dalinant iš 2
-const remainderOf2 = function (number) {
-  return number % 2;
-}
-
-const remaindersOf2 = numbers.map(remainderOf2);
-console.log(remaindersOf2);
-
-// Sukurkite kainų masyvą ,pagal masyvą flats, panaudodami butų kainas eurais ir paversdami į dolerius
-const convertFlatPrice = function (flat) {
-  return flat.price * EUR_USD;
-}
-
-const pricesUSD = flats.map(convertFlatPrice);
-console.log(pricesUSD);
-
-// Sukurkite butų masyvą, pagal masyvą flats, paversdami kainas doleriais, 
-// NEPAKEIČIANT ITERUOJAMO MASYVO OBJEKTŲ
-const changeFlatPrice = function (flat) {
-  const flatCopy = { ...flat };
-  flatCopy.price *= EUR_USD;
-
-  return flatCopy;
-}
-
-const flatsUS = flats.map(changeFlatPrice);
-console.table(flatsUS);
-console.table(flats);
-
-
-
+console.groupEnd();
