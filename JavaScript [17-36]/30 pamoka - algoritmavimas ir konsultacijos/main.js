6f2f73729850ef01b9342a9cbb8dc2ed2b34edf9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-11 20:37:09 +0300

    feat(30): recursion task

--- JavaScript [17-36]/30 pamoka - algoritmavimas ir konsultacijos/main.js
+++ JavaScript [17-36]/30 pamoka - algoritmavimas ir konsultacijos/main.js
@@ -1,14 +1,61 @@
-console.group('17. Sukurti funkciją, kuri ima masyvą ir grąžina didžiausią skaičių masyve.');
-console.log('---');
+console.group('3. Atspausdinti failu struktūrą naudojant rekursiją')
 {
-  const numbers = [1, 2, -2, 6, -5, 9, 1.02, 45, -69, 77, -12, 2, 8, -2, -4, 59, 7, -3];
-  const arrMax = (arr) => Math.max(...arr);
-
-  console.log({
-    numbers,
-    result: arrMax(numbers)
-  });
+  const myFolder = {
+    type: 'folder',
+    name: 'Javascript kursai',
+    files: [
+      {
+        type: 'file',
+        name: 'komandos.txt'
+      },
+      {
+        type: 'folder',
+        name: '1 savaitė',
+        files: [
+          {
+            type: 'folder',
+            name: '1 pamoka',
+            files: [
+              {
+                type: 'file',
+                name: 'index.html'
+              },
+              {
+                type: 'file',
+                name: 'main.css'
+              },
+            ]
+          },
+          {
+            type: 'folder',
+            name: '2 pamoka',
+            files: [
+              {
+                type: 'file',
+                name: 'index.html'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+  function printFiles(file, prefix = '') {
+    const isFolder = file.type === 'folder';
+    const formattedFileName = `${prefix}${file.name}${(isFolder ? ' /' : '')}`;
+    console.log(formattedFileName);
+    if (isFolder) {
+      file.files.forEach(childFile => printFiles(childFile, prefix + '--'));
+    }
+  }
+  printFiles(myFolder);
+  // Javascript kursai
+  // --file
+  // --folder /
+  // ----1 pamoka /
+  // ------index.html
+  // ------main.css
+  // ----2 pamoka /
+  // ------index.html
 }
-console.log('---');
-
-// 20:15
+console.groupEnd();
