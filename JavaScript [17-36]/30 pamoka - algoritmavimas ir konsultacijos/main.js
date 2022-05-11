9ab3d3761d3c912b6068fbddc6cb33400933c234
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-11 21:56:31 +0300

    feat(30): recursion-4 iliustration

--- JavaScript [17-36]/30 pamoka - algoritmavimas ir konsultacijos/main.js
+++ JavaScript [17-36]/30 pamoka - algoritmavimas ir konsultacijos/main.js
@@ -59,3 +59,53 @@ console.group('3. Atspausdinti failu struktūrą naudojant rekursiją')
   // ------index.html
 }
 console.groupEnd();
+
+console.group('4. Parašyti funkciją, kuri surastų objektuose arba masyvuose visas skaitines reikšmes, ir jas sudėtų');
+{
+  const calcSum = (numberStructure) => {
+    console.group('function called with: ' + numberStructure);
+    let returnValue;
+    if (numberStructure instanceof Array) {
+      returnValue = numberStructure.reduce((sum, el) => sum + calcSum(el), 0)
+    } else if (numberStructure instanceof Object) {
+      const arr = Object.values(numberStructure);
+      returnValue = arr.reduce((sum, el) => sum + calcSum(el), 0)
+    } else {
+      returnValue = numberStructure
+    }
+    console.log('returned:', returnValue);
+    console.groupEnd()
+    return returnValue;
+  }
+
+  const data = [
+    1,
+    5,
+    [
+      4,
+      5,
+      9,
+      {
+        q: 7,
+        z: [4, 8, 9]
+      }
+    ],
+    8,
+    {
+      a: 7,
+      b: 9,
+      h: [
+        4,
+        5,
+        {
+          j: 7,
+          p: 8,
+          z: [7, 7, 7]
+        }
+      ]
+    }
+  ];
+
+  console.log(calcSum(data));
+}
+console.groupEnd();
