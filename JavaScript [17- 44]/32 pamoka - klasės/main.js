1df3818d5d92ba4f7570eed2f265bf06b1beda5b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-16 19:31:08 +0300

    feat(32): Student addMark and calcAvg methods

--- JavaScript [17- 44]/32 pamoka - klasės/main.js
+++ JavaScript [17- 44]/32 pamoka - klasės/main.js
@@ -1,27 +1,44 @@
 class Student {
   constructor(name, surname, age, course) {
-    if (course < 1) {
-      console.error('Neteisingas kurso formatas');
-    }
-
     this.name = name;
     this.surname = surname;
     this.age = age;
     this.course = course;
+    this.marks = [];
   }
 
-  // funkcija aprašyta viduje klasės yra vadinama metodu
   printFullName() {
     console.log(this.name, this.surname);
   }
+
+  addMark(mark) {
+    this.marks.push(mark);
+  }
+
+  calcAvg() {
+    if (this.marks.length < 1) {
+      return 'Nėra pažymių';
+    }
+    return this.marks.reduce((sum, m) => sum + m) / this.marks.length;
+  }
 }
 
-const students = [
-  new Student('Serbentautas', 'Bordiūras', 19, 1),
-  new Student('Veranda', 'Kolidorė', 21, -5),
-  new Student('Stalčius', 'Komodauskas', 22),
-  new Student('Jantinga', 'Mantinka', 21, 1),
-];
+const stud1 = new Student('Veranda', 'Kolidorė', 21, 2);
+const stud2 = new Student('Serbentautas', 'Bordiūras', 19, 1);
+const stud3 = new Student('Stalčius', 'Komodauskas', 22, 3);
+const stud4 = new Student('Jantinga', 'Mantinka', 21, 1);
+
+const students = [stud1, stud2, stud3, stud4];
+//                 0       1      2     3
 
 
-students.forEach(student => student.printFullName())
+stud1.course = 77;
+
+
+console.log(stud1.course);
+console.log(stud1.calcAvg());
+stud1.addMark(7);
+stud1.addMark(8);
+console.log(stud1.calcAvg());
+stud1.addMark(9);
+console.log(stud1.calcAvg());
