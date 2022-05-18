f42e63d94ec4311f93854920aaf867e3d3d50814
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-18 20:53:32 +0300

    chore(34): added and removed comments

--- JavaScript [17- 44]/34 pamoka - oop - kompozicija 2/js/data/modules.js
+++ JavaScript [17- 44]/34 pamoka - oop - kompozicija 2/js/data/modules.js
@@ -2,6 +2,10 @@ import { softwareSystemsModules } from './info/software-systems.js';
 
 const modules = [
   ...softwareSystemsModules,
+  // ...softwareSystemsModules,
+  // ...lawModules,
+  // ...biologyModules,
+  // ...chemistryModules,
 ];
 
 export default modules;
--- JavaScript [17- 44]/34 pamoka - oop - kompozicija 2/js/main.js
+++ JavaScript [17- 44]/34 pamoka - oop - kompozicija 2/js/main.js
@@ -10,17 +10,4 @@ const studentsInfoSoftwareSystems = [
   new Student('Kompiuterė', 'Komplailerytė', softwareSystems.id),
 ];
 
-/*
-  aplanke 'data/' sukurkite failą 'modules.js'
-    * šiame faile importuokite <softwareSystemsModules> iš failo 'data/info/software-systems.js'
-    * sukurkite masyvą <modules>
-    * į masyvą <modules> įdėkite <softwareSystemsModules>
-    * eksportuokite masyvą <modules>
-    
-  'main.js' (šiame) faile
-    * importuokite <modules> iš failo 'modules.js'
-    * atspausdinkite naudodami <console.table>
-*/
-
-console.table(modules);
 
