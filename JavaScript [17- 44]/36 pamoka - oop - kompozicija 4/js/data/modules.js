af1b7404ab3c5a9519032f1396c8325b37d616c8
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-23 18:04:28 +0300

    init(36): composition 4

---
+++ JavaScript [17- 44]/36 pamoka - oop - kompozicija 4/js/data/modules.js
@@ -0,0 +1,13 @@
+import { softwareSystemsModules } from './info-math/software-systems.js';
+import { mathematicsAndItsApplicationsModules } from './info-math/mathematics-and-its-applications.js';
+import { dataScienceModules } from './info-math/data-science.js';
+import { economicsAndBusinessModules } from "./eco/business-law.js";
+
+const modules = [
+  ...softwareSystemsModules,
+  ...dataScienceModules,
+  ...economicsAndBusinessModules,
+  ...mathematicsAndItsApplicationsModules,
+];
+
+export default modules;
---
+++ JavaScript [17- 44]/36 pamoka - oop - kompozicija 4/js/data/study-programs.js
@@ -0,0 +1,13 @@
+import softwareSystems from "./info-math/software-systems.js";
+import mathematicsAndItsApplications from "./info-math/mathematics-and-its-applications.js";
+import dataScience from "./info-math/data-science.js";
+import businessAndLaw from "./eco/business-law.js";
+
+const studyPrograms = [
+  softwareSystems,
+  mathematicsAndItsApplications,
+  dataScience,
+  businessAndLaw,
+];
+
+export default studyPrograms;
