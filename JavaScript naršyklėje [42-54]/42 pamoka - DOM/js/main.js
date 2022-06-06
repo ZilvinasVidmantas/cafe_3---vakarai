605d26e44ab160062c9a879cb760e56694dbfc9b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-06 21:27:45 +0300

    task-done(42): document.querySelector

--- JavaScript naršyklėje [42-54]/42 pamoka - DOM/js/main.js
+++ JavaScript naršyklėje [42-54]/42 pamoka - DOM/js/main.js
@@ -1,21 +1,26 @@
 const firstSection = document.querySelector('section');
-firstSection.style.background = 'red';
+// firstSection.style.background = 'red';
 firstSection.className = 'mano-klase'
-console.log(firstSection);
 
 const firstContainer = document.querySelector('.container');
 firstContainer.classList.add('bg-success');
-console.log(firstContainer);
 
 /*
   1. Suraskite pirmą h1 elementą puslapyje ir uždėkite 'primary' spalvos sienelę naudodami bootstrap
   klases.
+*/
+const firstHeading = document.querySelector('h1');
+firstHeading.className = 'border border-primary';
 
+/*
   2. Suraskite pirmą h2 elementą puslapyje ir uždėkite 'solid 2px magenta' sienelę, naudodami style 
   atributą.
-
-  3. Suraskite pirmą h2 elementą firstSection'e ir uždėkite 'bg-info' klasę.
-
-  21:10
-  21:20
 */
+const firstH2Heading = document.querySelector('h2');
+firstH2Heading.style.border = 'solid 2px red';
+
+/*
+  3. Suraskite pirmą h2 elementą firstSection'e ir uždėkite 'bg-info' klasę.
+*/
+const firstH2HeadingInSection = firstSection.querySelector('h2');
+firstH2HeadingInSection.className = 'bg-info';
