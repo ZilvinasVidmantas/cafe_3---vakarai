ccf3249a020fe30b54eb623bac30c76949036ae3
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-06 21:57:01 +0300

    feat(42): fadeUp example using querySelectorAll

--- JavaScript naršyklėje [42-54]/42 pamoka - DOM/js/main.js
+++ JavaScript naršyklėje [42-54]/42 pamoka - DOM/js/main.js
@@ -1,6 +1,8 @@
-const firstSection = document.querySelector('section');
-// firstSection.style.background = 'red';
-firstSection.className = 'mano-klase'
+const fadeUps = [...document.querySelectorAll('.fade-up')];
 
-const firstContainer = document.querySelector('.container');
-firstContainer.classList.add('bg-success');
+const fadeAnimDelayBase = 600;
+const fadeAnimDelayDelta = 400
+
+fadeUps.forEach((fadeUp, i) => {
+  setTimeout(() => fadeUp.classList.add('show'), fadeAnimDelayBase + i * fadeAnimDelayDelta)
+});
