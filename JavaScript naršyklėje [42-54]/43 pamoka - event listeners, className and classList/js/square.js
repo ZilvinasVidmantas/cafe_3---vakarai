85eba62493e441ab16a05ae7384d0208a42f5f47
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-07 19:18:16 +0300

    task(43): EvetListener task

--- JavaScript naršyklėje [42-54]/43 pamoka - event listeners, className and classList/js/square.js
+++ JavaScript naršyklėje [42-54]/43 pamoka - event listeners, className and classList/js/square.js
@@ -6,3 +6,15 @@ const changeSquareColorToRed = () => {
 };
 
 btnChangeToRed.addEventListener('click', changeSquareColorToRed);
+
+/*
+  Sukurkite logiką, kad paspaudus mygtuką:
+    <button class="btn btn-primary">Keisti foną į mėlyną</button>
+  Kvadratėlio spalva taptų mėlyna:
+    * Mygtukui uždėti klasę, kurią naudosime mygtuko suradimui, ji turi prasidėti 'js-*'
+    * square.js faile surasti šį mygtuką, pasitikrinti konsolėje
+    * Sukurti funkciją kuri pakeis kvadratėlio spalvą į mėlyną, kuri tik spausintų tektą į konsolę
+    * Uždėti ant mėlyno mygtuko kintamojo EventListener'į, ant paspaudimo ('click')
+    * Implementuoji funkcijos logiką, kuri keičia kvadratėlio spalvą
+    * Pa'like'int chat'e 'atlikau' žinutę
+*/
