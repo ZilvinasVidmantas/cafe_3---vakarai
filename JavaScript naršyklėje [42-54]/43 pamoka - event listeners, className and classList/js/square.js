7871bdcb3daf460b9fefd5d9c14892110e4c9888
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-07 19:50:38 +0300

    task-done(43): EventListener task with multiple class checks

--- JavaScript naršyklėje [42-54]/43 pamoka - event listeners, className and classList/js/square.js
+++ JavaScript naršyklėje [42-54]/43 pamoka - event listeners, className and classList/js/square.js
@@ -1,22 +1,35 @@
 const square = document.querySelector('.js-square');
 const btnChangeToRed = document.querySelector('.js-btn-change-to-red');
 const btnChangeToBlue = document.querySelector('.js-btn-change-to-blue');
+const btnChangeToGreen = document.querySelector('.js-btn-change-to-green');
+
+const bgClasses = ['bg-blue', 'bg-red', 'bg-green'];
+
+const removeBgClasses = () => {
+  bgClasses.forEach(cls => {
+    if (square.classList.contains(cls)) {
+      square.classList.remove(cls);
+    }
+  });
+};
 
 const changeSquareColorToRed = () => {
-  if (square.classList.contains('bg-blue')) {
-    square.classList.remove('bg-blue');
-  }
+  removeBgClasses();
   square.classList.add('bg-red');
 };
 
 const changeSquareColorToBlue = () => {
-  if (square.classList.contains('bg-red')) {
-    square.classList.remove('bg-red');
-  }
+  removeBgClasses();
   square.classList.add('bg-blue');
+};
+
+const changeSquareColorToGreen = () => {
+  removeBgClasses();
+  square.classList.add('bg-green');
 };
 
 btnChangeToRed.addEventListener('click', changeSquareColorToRed);
 btnChangeToBlue.addEventListener('click', changeSquareColorToBlue);
+btnChangeToGreen.addEventListener('click', changeSquareColorToGreen);
 
 // Sukurkite papildomą mygtuką, kuris pakeistų kvadračiuko spalvą į žalią
