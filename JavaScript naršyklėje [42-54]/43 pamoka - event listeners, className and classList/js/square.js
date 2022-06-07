5e19fe1ea91bde8b440aff32775ba55ca807dab5
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-07 20:31:05 +0300

    feat(43): square width EventListener

--- JavaScript naršyklėje [42-54]/43 pamoka - event listeners, className and classList/js/square.js
+++ JavaScript naršyklėje [42-54]/43 pamoka - event listeners, className and classList/js/square.js
@@ -2,32 +2,48 @@ const square = document.querySelector('.js-square');
 const btnChangeToRed = document.querySelector('.js-btn-change-to-red');
 const btnChangeToBlue = document.querySelector('.js-btn-change-to-blue');
 const btnChangeToGreen = document.querySelector('.js-btn-change-to-green');
+const btnIncWidth = document.querySelector('.js-btn-inc-width');
 
-const bgClasses = ['bg-blue', 'bg-red', 'bg-green'];
+// color handlers
+{
+  const bgClasses = ['bg-blue', 'bg-red', 'bg-green'];
 
-const removeBgClasses = () => {
-  bgClasses.forEach(cls => {
-    if (square.classList.contains(cls)) {
-      square.classList.remove(cls);
-    }
-  });
-};
+  const removeBgClasses = () => {
+    bgClasses.forEach(cls => {
+      if (square.classList.contains(cls)) {
+        square.classList.remove(cls);
+      }
+    });
+  };
 
-const changeSquareColorToRed = () => {
-  removeBgClasses();
-  square.classList.add('bg-red');
-};
+  const changeSquareColorToRed = () => {
+    removeBgClasses();
+    square.classList.add('bg-red');
+  };
 
-const changeSquareColorToBlue = () => {
-  removeBgClasses();
-  square.classList.add('bg-blue');
-};
+  const changeSquareColorToBlue = () => {
+    removeBgClasses();
+    square.classList.add('bg-blue');
+  };
 
-const changeSquareColorToGreen = () => {
-  removeBgClasses();
-  square.classList.add('bg-green');
-};
+  const changeSquareColorToGreen = () => {
+    removeBgClasses();
+    square.classList.add('bg-green');
+  };
 
-btnChangeToRed.addEventListener('click', changeSquareColorToRed);
-btnChangeToBlue.addEventListener('click', changeSquareColorToBlue);
-btnChangeToGreen.addEventListener('click', changeSquareColorToGreen);
+  btnChangeToRed.addEventListener('click', changeSquareColorToRed);
+  btnChangeToBlue.addEventListener('click', changeSquareColorToBlue);
+  btnChangeToGreen.addEventListener('click', changeSquareColorToGreen);
+}
+
+// size handlers
+{
+  let { width } = square.getBoundingClientRect();
+
+  const increaseSquareWidth = () => {
+    width += 50;
+    square.style.width = width + 'px';
+  }
+
+  btnIncWidth.addEventListener('click', increaseSquareWidth);
+}
