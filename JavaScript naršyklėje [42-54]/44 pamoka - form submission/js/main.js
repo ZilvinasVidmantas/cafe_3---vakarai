c09fe67032eaa2879b41478d718470e7cb3feaf2
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-08 20:31:41 +0300

    task(44): register form task

--- JavaScript naršyklėje [42-54]/44 pamoka - form submission/js/main.js
+++ JavaScript naršyklėje [42-54]/44 pamoka - form submission/js/main.js
@@ -19,7 +19,30 @@ const handleRegisterFormSubmit = (event) => {
 registerForm.addEventListener('submit', handleRegisterFormSubmit);
 
 
-// 5min
-// 10min
+const formData = new FormData(registerForm);
+formData.delete('name');
 
-// 20:10
+
+/*
+    Sukurkite formos vaizdą
+      * formos laukai
+        * paštas -                    input,   type="email"
+        * slaptažodis -               input,   type="password"
+        * slaptažodžio pakartojimas - input,   type="password"
+        * vardas -                    input,   type="text"
+        * pavardė -                   input,   type="text"
+        * miestas -                   select,  5 pasirinkimai
+        * lytis -                     3 input, type="radio":    vyras, moteris ir kita
+        * esami įgūdžiai -            5 input, type="checkbox": HTML, CSS, JS, React, Node
+        * žinutė -                    textarea 
+    
+    Patarimai:
+      Vaizdą pradėkite kurti nuo mažiausio ekrano dydžio
+      Naudokite Bootsrap formos pavyzdžius: https://getbootstrap.com/docs/5.0/forms/overview/
+      Pritaikykite formos vaizdą visiems ekrano dydžiams: https://getbootstrap.com/docs/5.0/layout/grid/
+      Nepamirškite įvesties laukams pridėti ANGLIŠKAS name(atributo) reikšmes
+
+    Sudarę formos vaizdą, suraskite ją main.js faile, ir įgalinkite šios formos duomenų atspausdinimą
+    konsolėje, kuome ši forma yra submit'inama
+
+*/
