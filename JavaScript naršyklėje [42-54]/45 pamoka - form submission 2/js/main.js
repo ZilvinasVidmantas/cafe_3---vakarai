de473f9b5541883f832ea27d02b705053320ec7c
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-09 20:21:24 +0300

    feat(45): error formatted

--- JavaScript naršyklėje [42-54]/45 pamoka - form submission 2/js/main.js
+++ JavaScript naršyklėje [42-54]/45 pamoka - form submission 2/js/main.js
@@ -3,6 +3,8 @@ const exampleForm = document.querySelector('.js-example-form');
 const exampleFormResultContainer = document.querySelector('.js-example-form-result');
 const registrationForm = document.querySelector('.js-registration-form');
 
+const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 const getFormValues = (form) => {
   const formData = new FormData(form);
   const formValues = {};
@@ -22,10 +24,57 @@ const handleExampleFormSubmit = (event) => {
   exampleFormResultContainer.innerHTML = JSON.stringify(values, null, 4);
 };
 
+const formatRegisterErrors = (values) => {
+  const errors = {
+    email: [],
+    password: [],
+    passwordConfirmation: []
+  };
+
+  // email
+  if (values.email === undefined || values.email === '') {
+    errors.email.push('laukas negali būti tuščias');
+  }
+  if (!EMAIL_REGEX.test(values.email)) {
+    errors.email.push('neteisingas el. pašto formatas');
+  }
+
+  // password
+  if (values.password === undefined || values.password === '') {
+    errors.password.push('laukas negali būti tuščias');
+  }
+
+  // passwordConfirmation
+  if (values.passwordConfirmation === undefined || values.passwordConfirmation === '') {
+    errors.passwordConfirmation.push('laukas negali būti tuščias');
+  }
+
+  return errors;
+}
+
+const validateRegisterErrors = (errors) => {
+  const errorValues = Object.values(errors);
+
+  for (let i = 0; i < errorValues.length; i++) {
+    if (errorValues[i].length > 0) return false;
+  }
+
+  return true;
+}
+
 const handleRegister = (event) => {
   event.preventDefault();
   const values = getFormValues(event.target);
-  console.log(JSON.stringify(values, null, 4));
+  const errors = formatRegisterErrors(values);
+  const isValid = validateRegisterErrors(errors);
+
+  if (isValid) {
+    console.log('Formos duomenys teisingi!');
+  } else {
+    console.log('Formoje yra klaidų');
+    console.log(JSON.stringify(errors, null, 4))
+  }
+
 };
 
 exampleForm.addEventListener('submit', handleExampleFormSubmit);
