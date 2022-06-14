2fd7815d04ad49c978ce79b39017ab321f672f70
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-14 19:20:25 +0300

    task(47): add product form

--- JavaScript naršyklėje [42-54]/47 pamoka - content rendering/js/main.js
+++ JavaScript naršyklėje [42-54]/47 pamoka - content rendering/js/main.js
@@ -1,2 +1,3 @@
 import './scripts/example-form.js';
 import './scripts/register-form.js';
+import './scripts/product-create-form.js';
---
+++ JavaScript naršyklėje [42-54]/47 pamoka - content rendering/js/scripts/product-create-form.js
@@ -0,0 +1,35 @@
+/*
+  Sukurkite form'ą pridėti produktui:
+
+  1. Sukurkite vaizdą visiems ekrano dydžiams
+  Produkto pridėjimo laukai(fields)
+    * title: input[text]
+    * category: select
+      * food: option
+      * clothes: option
+      * other: option
+    * price: input[number]
+    * amount: input[number]
+    * condition:
+      * new: input[radio]   
+      * used: input[radio]   
+
+  2. Sukurkite skript'ą, kuris atliktu validavimą (analogišką registracijos formai) ir duomenų 
+    atspausdinimą konsolėje
+
+    validuojamos savybės:
+      * title:
+        * privaloma
+        * min 2 raidės
+        * max 32 raidės
+      * category:
+        * privaloma
+      * price:
+        * privaloma
+        * turi būti aprašyta ne daugiau kaip 2 skaičiai po kablelio
+      * amount:
+        * privaloma
+        * turi būti sveikas skaičius
+      * condition:
+        * privaloma        
+*/
