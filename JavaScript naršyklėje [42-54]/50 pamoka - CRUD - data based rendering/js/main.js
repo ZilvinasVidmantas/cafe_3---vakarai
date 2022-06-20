1fe763ea721de7e79fa9f0aa7b2c076f8d204f34
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-20 18:10:27 +0300

    init(50): crud - data based rendering

---
+++ JavaScript naršyklėje [42-54]/50 pamoka - CRUD - data based rendering/js/main.js
@@ -0,0 +1,18 @@
+
+/*
+  Paaiškinkite FormComponent klasės metodus, naudodami savo sukurtą iliustraciją.
+    Nupieškite iliustraciją, su bet kokia programa ir per 2-4 minutes paaiškinkite 
+    funkcijų veikimą, nuadojant iliustraciją
+      constructor
+        11, 21
+      get values();
+        12
+      initFields
+        13
+      initEventListener
+        16
+      clearFieldsErrors
+       18
+      displayFormErrors
+       20
+*/
