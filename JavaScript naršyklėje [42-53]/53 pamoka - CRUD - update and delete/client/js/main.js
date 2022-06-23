f60d7e6d6ec3f8519b2f2c6e1e683274d464d628
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-23 21:19:26 +0300

    feat(53): update example usiing bootstrap modal

--- JavaScript naršyklėje [42-53]/53 pamoka - CRUD - update and delete/client/js/main.js
+++ JavaScript naršyklėje [42-53]/53 pamoka - CRUD - update and delete/client/js/main.js
@@ -4,6 +4,29 @@ import ApiService from "./helpers/api-service.js";
 
 // Surandame elementę, į kurį dėsime TodoItem'us
 const todoList = document.querySelector('.js-todo-list'); // <div class="js-todo-list todo-list"></div>
+const updateFormModal = new bootstrap.Modal('#update-form-modal');
+const updateTitleField = document.querySelector('#update-title-input');
+const updateCompletedField = document.querySelector('#update-completed-input');
+const btnUpdateTodo = document.querySelector('#btn-update-todo');
+let editableTodoId = null;
+let editableTodoItemText = null;
+let editableCheckbox = null;
+
+const updateTodo = async () => {
+  const { title, completed } = await ApiService.updateTodo({
+    id: editableTodoId,
+    title: updateTitleField.value,
+    completed: updateCompletedField.checked,
+  });
+
+  editableTodoItemText.innerText = title;
+  editableCheckbox.classList.remove('checked');
+  if (completed) editableCheckbox.classList.add('checked');
+
+  editableTodoId = null;
+  editableTodoItemText = null;
+  editableCheckbox = null;
+}
 
 // Funkcija, kuri priimą objektą, ir pagal priimtą objektą, sukurią naują TodoItem'ą į įdeda jį
 // į "todoList" kintamajį
@@ -19,7 +42,6 @@ const displayTodoItem = ({
   checkbox.className = 'checkbox';  // <div class="checkbox"></div>
   if (completed) checkbox.classList.add('checked'); // <div class="checkbox checked"></div>
   checkbox.addEventListener('click', async () => {
-    console.log({ title, completed });
     await ApiService.updateTodo({
       id,
       completed: !checkbox.classList.contains('checked')
@@ -32,6 +54,19 @@ const displayTodoItem = ({
   todoItemText.className = 'todo-list__item__text'; // <div class="todo-list__item__text"></div>
   todoItemText.innerText = title; // <div class="todo-list__item__text">{{ title }}</div>
 
+  const btnUpdate = document.createElement('button'); // <button></button>
+  btnUpdate.className = 'button'; // <button class="button"</button>
+  btnUpdate.innerText = '↻'; // <button class="button">✖</button>
+  btnUpdate.addEventListener('click', () => {
+    updateTitleField.value = todoItemText.innerText;
+    updateCompletedField.checked = checkbox.classList.contains('checked');
+    editableTodoId = id;
+    editableTodoItemText = todoItemText;
+    editableCheckbox = checkbox;
+
+    updateFormModal.show();
+  });
+
   const btnDelete = document.createElement('button'); // <button></button>
   btnDelete.className = 'button'; // <button class="button"</button>
   btnDelete.innerText = '✖'; // <button class="button">✖</button>
@@ -43,6 +78,7 @@ const displayTodoItem = ({
   todoItem.append(  // <div class="todo-list__item">
     checkbox,       //   <div class="checkbox checked"></div>
     todoItemText,   //   <div class="todo-list__item__text">{{ text }}</div>
+    btnUpdate,
     btnDelete       //   <button class="button">✖</button>
   );                // </div>
 
@@ -69,3 +105,16 @@ const formAddTodo = new FormComponent(
 // Pradinių duomenų parsiuntimas
 const todos = await ApiService.fetchTodos();
 todos.forEach(displayTodoItem);
+
+btnUpdateTodo.addEventListener('click', () => {
+  updateTodo();
+  updateFormModal.hide();
+});
+
+
+/* 
+  Data-based-rendering
+    * Atvaizdavimui skirti duomenys saugomi bendrai pasiekiamoje vietoje
+    * 1 funkcija skirta atvaizdavimui, kuri kviečiama kiekvieną kartą kai keičiasi duomenys
+    * Visų EventLister'ių funkcijos keičia duomenis
+*/
