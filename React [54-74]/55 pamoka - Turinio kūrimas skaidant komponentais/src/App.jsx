271178730c3772271f59cb85314732f1efc6b84a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-28 18:55:42 +0300

    feat(55): first custom component example

--- React [54-74]/55 pamoka - Turinio kūrimas skaidant komponentais/src/App.jsx
+++ React [54-74]/55 pamoka - Turinio kūrimas skaidant komponentais/src/App.jsx
@@ -1,6 +1,15 @@
+import Button from './button';
+
 function App() {
+
   return (
-    <h1>Čia yra app'sas</h1>
+    <>
+      <h1>Čia yra app'sas</h1>
+      <Button color="red">Tekstas viduje mygtuko</Button>
+      <Button>Click</Button>
+      <Button type="submit" >Submit</Button>
+      <Button>Send</Button>
+    </>
   );
 }
 
