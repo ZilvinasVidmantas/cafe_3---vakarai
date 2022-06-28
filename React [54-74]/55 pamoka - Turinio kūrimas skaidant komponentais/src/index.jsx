994f75767d0b17a12a74ef72a52da9d02dd53494
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-28 18:16:44 +0300

    init(55): Component structuring

---
+++ React [54-74]/55 pamoka - Turinio kūrimas skaidant komponentais/src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  /* 
+    Kviečia aplikaciją 2 kartus, 1 pateikdamas jums, kitą pasilikdamas savo vidinei logikai.
+    Tuomet, kiekvieno aplikacijos ciklo metu, lygina jūsų aplikacijos su menama aplikacija versijas
+    ieškodamas nekorektiškų ir prie kladų vedančių atvejų. Suradęs tokius atvejus, praneša apie
+    klaidas konsolėje, arba grubių klaidų atveju nutraukią jūsų aplikaciją.
+   */
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
