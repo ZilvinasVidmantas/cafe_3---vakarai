62f985a93c96dedb82c83d0dfe766e27a0cc5b68
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-30 19:50:32 +0300

    feat(57): cats-grid

--- React [54-74]/57 pamoka - savarankiškas darbas struktūrizuojant komponentus/src/components/concrete/cat-card/index.jsx
+++ React [54-74]/57 pamoka - savarankiškas darbas struktūrizuojant komponentus/src/components/concrete/cat-card/index.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Img from '../../abstracts/img';
+import Card from '../../abstracts/card';
+import classes from './index.module.scss';
 
 const CatCard = ({ tags, imgSrc }) => {
   return (
-    <div>
-      <div>{tags.join(', ')}</div>
-      <div>{imgSrc}</div>
-    </div>
+    <Card>
+      <div className={classes.imgContainer}>
+        <Img
+          src={imgSrc}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </div>
+      <div className={classes.tagContainer}>
+        {tags.map((tag) => <div key={tag} className={classes.chip}>{tag}</div>)}
+      </div>
+    </Card>
   )
 }
 
 export default CatCard;
-
-
-// 18:50
