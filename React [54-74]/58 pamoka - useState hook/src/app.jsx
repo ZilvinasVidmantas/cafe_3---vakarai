849cccd9cf4aa285c67fb859c0fd9195c57f7947
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-07-04 18:04:23 +0300

    init(58): useState hook

---
+++ React [54-74]/58 pamoka - useState hook/src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CatsSection from './components/concrete/cats-section';
+import PersonSection from './components/concrete/person-section';
+
+function App() {
+
+  return (
+    <>
+      <PersonSection />
+      <CatsSection />
+    </>
+  );
+}
+
+export default App;
