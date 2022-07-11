86e5f5f89fd40ffaa76fd3988eaf05419667a0bf
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-07-11 18:44:35 +0300

    init(60): Material UI intro

---
+++ React [54-74]/60 pamoka - Material UI/src/app.jsx
@@ -0,0 +1,15 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+const App = () => {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained" color="secondary">Contained</Button>
+      <Button variant="contained" color="primary">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+}
+
+export default App;
