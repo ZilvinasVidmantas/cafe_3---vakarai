2b0fa5bc714da535b1925e8a7636ccb6bc1d945e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-07-11 20:47:17 +0300

    feat(60): reponsive navbar UI created

--- React [54-74]/60 pamoka - Material UI/src/components/navbar.jsx
+++ React [54-74]/60 pamoka - Material UI/src/components/navbar.jsx
@@ -3,35 +3,48 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+const pages = ['Pagrindinis', 'Gamybos eiga', 'Mano lentyna'];
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{
+              mr: 2,
+              display: { sm: 'none' }
+            }}
           >
             <MenuIcon />
           </IconButton>
-          {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography> */}
+
+          <Box sx={{ display: 'flex', gap: 2 }}>
+            {pages.map((pageTitle) => (
+              <Typography
+                key={pageTitle}
+                variant="h6"
+                component="div"
+                sx={{
+                  display: { xs: 'none', sm: 'block' }
+                }}
+              >
+                {pageTitle}
+              </Typography>
+            ))}
+          </Box>
+
+          <ShoppingBasketIcon />
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
 
-/*
-  20:05
-
-
-*/
