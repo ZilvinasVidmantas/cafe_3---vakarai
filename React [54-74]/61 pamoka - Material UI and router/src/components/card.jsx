c4460da19f832b231e1b2180f438713df0d80646
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-07-12 20:22:14 +0300

    feat(61): Mug grid UI improvements

--- React [54-74]/61 pamoka - Material UI and router/src/components/card.jsx
+++ React [54-74]/61 pamoka - Material UI and router/src/components/card.jsx
@@ -11,13 +11,24 @@ import {
 
 const MugCard = ({ title, category, img, description, }) => {
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        image={img}
-        alt=""
-      />
-      <CardContent sx={{ p: 2 }}>
+    <Card sx={{
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <Box sx={{
+        position: 'relative',
+        width: '100%',
+        pt: '95%',
+      }}>
+        <CardMedia
+          component="img"
+          image={img}
+          alt=""
+          sx={{ position: 'absolute', top: 0, left: 0, height: '100%', width: '100%' }}
+        />
+      </Box>
+      <CardContent sx={{ p: 2, flexGrow: 1 }}>
         <Box sx={{
           display: 'flex',
           justifyContent: 'space-between',
@@ -27,9 +38,22 @@ const MugCard = ({ title, category, img, description, }) => {
           <Typography variant="h5" component="div">{title}</Typography>
           <Typography variant="subtitle" component="div">{category}</Typography>
         </Box>
-        <Typography variant="body2" color="text.secondary">{description}</Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            display: '-webkit-box',
+            WebkitLineClamp: 4,
+            WebkitBoxOrient: 'vertical',
+          }}
+        >
+          {description}
+        </Typography>
       </CardContent>
-      <CardActions sx={{ p: 2 }}>
+
+      <CardActions sx={{ p: 2, alignSelf: 'center' }}>
         <Button size="small" variant="contained">Peržiūrėti</Button>
         <Button size="small" variant="contained">Dėti į krepšelį</Button>
       </CardActions>
