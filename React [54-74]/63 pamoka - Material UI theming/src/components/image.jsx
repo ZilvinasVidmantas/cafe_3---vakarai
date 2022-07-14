5a948254d0f38e489b3000d6afd0ff233cbb1c1e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-07-14 19:56:14 +0300

    feat(63): theme integration

---
+++ React [54-74]/63 pamoka - Material UI theming/src/components/image.jsx
@@ -0,0 +1,8 @@
+import { styled } from "@mui/material";
+
+export const Image = styled('img')({
+  objectFit: 'cover',
+  objectPosition: 'center',
+  height: '100%',
+  width: '100%',
+});
--- React [54-74]/63 pamoka - Material UI theming/src/pages/home-page/index.jsx
+++ React [54-74]/63 pamoka - Material UI theming/src/pages/home-page/index.jsx
@@ -1,84 +1,30 @@
 import * as React from 'react';
-import {
-  Box,
-  styled,
-  Typography,
-} from '@mui/material';
-import { Link } from 'react-router-dom';
-
-const Background = styled(Box)({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  height: '100%',
-  width: '100%',
-  order: 1,
-  objectFit: 'cover'
-});
-
-const ContentContainer = styled(Box)({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  height: '100%',
-  width: '100%',
-  order: 2,
-  display: 'flex',
-  justifyContent: 'flex-end',
-  alignItems: 'center',
-});
-
-const Content = styled(Box)(({ theme }) => ({
-  margin: theme.spacing(8, 8, 6, 0),
-  height: '80%',
-  width: 650,
-  color: theme.palette.common.white,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  gap: theme.spacing(6)
-}));
-
-const Image = styled('img')({
-  objectFit: 'cover',
-  objectPosition: 'center',
-  height: '100%',
-  width: '100%',
-});
+import { Box, Typography } from '@mui/material';
+import * as Home from './components';
 
 const HomePage = () => (
   <Box sx={{ height: '100vh', position: 'relative' }}>
-    <Background component="img" src="/home-cover.jpg" />
-    <ContentContainer>
-      <Content component="main">
+    <Home.Background component="img" src="/home-cover.jpg" />
+    <Home.ContentContainer>
+      <Home.Content component="main">
         <Typography
-          variant="h2"
           component="h1"
-          sx={{ letterSpacing: '0.08em', color: '#d99f2b' }}
-        >Tetos puodeliai</Typography>
+          variant="h2"
+          sx={(theme) => ({ letterSpacing: '0.08em', color: theme.palette.primary.main })}
+        >
+          Tetos puodeliai
+        </Typography>
+
         <Typography variant="h6" sx={{ textAlign: 'center' }}>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum tempora explicabo nostrum accusantium dolor dolores illo minus suscipit aliquam, inventore voluptate consectetur omnis labore, laboriosam accusamus! Deserunt mollitia doloribus laboriosam enim ut, numquam cumque quisquam tenetur maiores officiis autem necessitatibus magnam voluptate ipsa eveniet sequi neque in porro rem facere!
         </Typography>
+
         <Box sx={{ display: 'flex', gap: 3, height: 400 }}>
-          <Link to="/pottery" style={{ textDecoration: 'none' }}>
-            <Typography
-              className="title"
-              variant="h4"
-              sx={{ color: '#d99f2b', textAlign: 'center' }}
-            >Žiedimas</Typography>
-            <Image className="image" src="/pottery-aunt.jpg" />
-          </Link>
-          <Link to="/cup-shelf" style={{ textDecoration: 'none' }}>
-            <Typography
-              className="title"
-              variant="h4"
-              sx={{ color: '#d99f2b', textAlign: 'center' }}
-            >Puodelių lentyna</Typography>
-            <Image className="image" src="/cup.jpg" />
-          </Link>
+          <Home.ImageLink to="/pottery" src="/pottery-aunt.jpg">Žiedimas</Home.ImageLink>
+          <Home.ImageLink to="/cup-shelf" src="/cup.jpg">Puodelių lentyna</Home.ImageLink>
         </Box>
-      </Content>
-    </ContentContainer>
+      </Home.Content>
+    </Home.ContentContainer>
   </Box>
 );
 
