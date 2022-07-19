20d12d115421ba83ffc494852cd58eca2609215b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-07-19 19:34:26 +0300

    task(65): controlled field revision

--- React [54-74]/65 pamoka - Material UI įvesties laukai/src/pages/order-page/index.jsx
+++ React [54-74]/65 pamoka - Material UI įvesties laukai/src/pages/order-page/index.jsx
@@ -1,36 +1,59 @@
-import React from 'react';
+import * as React from 'react';
 import {
   Box, TextField, Paper, Typography, Button,
 } from '@mui/material';
 
-const OrderPage = () => (
-  <Box sx={{ pt: 7 }}>
-    <Paper
-      elevation={3}
-      sx={{
-        mt: 4,
-        p: 3,
-        width: 500,
-        mx: 'auto',
-      }}
-    >
-      <Box component="form">
-        <Typography
-          component="h1"
-          variant="h4"
-          align="center"
-          sx={{ pb: 3 }}
+/*
+  Įgalinkite dvipusį surišką tarp būsenos kintamojo ir paštui skirto TextField lauko
+
+*/
+const OrderPage = () => {
+  const [fullname, setFullname] = React.useState('Serbentautas Bordiūras');
+
+  return (
+    <Box sx={{ pt: 7 }}>
+      <Paper
+        elevation={3}
+        sx={{
+          mt: 4,
+          p: 3,
+          width: 500,
+          mx: 'auto',
+        }}
+      >
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 3,
+          }}
         >
-          Įveskite kontaktinius duomenis
-        </Typography>
-        <TextField label="name" fullWidth variant="filled" />
-        <Box sx={{ textAlign: 'center', pt: 3 }}>
+          <Typography component="h1" variant="h4" align="center">
+            Įveskite kontaktinius duomenis
+          </Typography>
+          <TextField
+            name="fullname"
+            label="Full name"
+            variant="filled"
+            fullWidth
+            onChange={(event) => setFullname(event.target.value)}
+            value={fullname}
+          />
+          <TextField
+            name="email"
+            type="email"
+            label="Email"
+            variant="filled"
+            fullWidth
+          />
           <Button type="submit" variant="contained" size="large">Užsakyti</Button>
         </Box>
-      </Box>
 
-    </Paper>
-  </Box>
-);
+      </Paper>
+    </Box>
+  );
+};
 
 export default OrderPage;
